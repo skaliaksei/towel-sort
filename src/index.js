de894053de80362d08a972d044de88aad7c5af05
Aliaksei Skakun <skaliaksei@gmail.com>
2020-10-17 18:22:19 +0300

    feat: create towel function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,54 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+    function recurseEven(arr) {
+        if (Array.isArray(arr)) {
+          let array = [];
+          for (let i = 0; i < arr.length; i++) {
+            array.push(arr[i]);
+          }
+          return array;
+        }
+        else {
+          return arr;
+        }
+
+      }
+
+    function recurseOdd(arr) {
+    if (Array.isArray(arr)) {
+        let array = [];
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            array.push(arr[i]);
+        }
+        return array;
+    }
+        else {
+            return arr;
+        }
+    }
+
+    console.log(matrix);
+
+    if (matrix != undefined) {
+        let resultArray = new Array();
+
+        for (let i = 0; i < matrix.length; i++) {
+            if (i % 2 == 0) {
+            resultArray = resultArray.concat(recurseEven(matrix[i]));
+            } else {
+            resultArray = resultArray.concat(recurseOdd(matrix[i]));
+            }
+        }
+
+        matrix = resultArray;
+        return matrix;
+
+    }
+    else {
+        matrix = [];
+        return matrix;
+    }
 }
